fcdbb1e8e7e476fd74b1bf0ae637870ed820cf68
Dimitri Dubois <dimitri.bleach@gmail.com>
2020-10-09 13:25:36 +0200

    Add files via upload

---
+++ src/lecture/MainTest.java
@@ -0,0 +1,19 @@
+package lecture;
+
+public class MainTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("boolean: "); Clavier.lire_boolean();
+        System.out.println("String: "); Clavier.lireString();
+        System.out.println("long: "); Clavier.lire_long();
+        System.out.println("float: "); Clavier.lire_float();
+        System.out.println("String: "); Clavier.lireString();
+        System.out.println("int: "); Clavier.lire_int();
+        System.out.println("short: "); Clavier.lire_short();
+        System.out.println("char: "); Clavier.lire_char();
+        System.out.println("double: "); Clavier.lire_double();
+        System.out.println("byte: "); Clavier.lire_byte();
+
+    }
+}
