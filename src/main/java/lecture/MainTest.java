923b2a196b3ffba268c7635ee74c50cfaf2e82af
Dim145 <dimitri.bleach@gmail.com>
2021-03-16 11:13:53 +0100

    change MainTest

--- src/main/java/lecture/MainTest.java
+++ src/main/java/lecture/MainTest.java
@@ -4,15 +4,15 @@ public class MainTest
 {
     public static void main(String[] args)
     {
-        System.out.println("boolean: "); Clavier.lire_boolean();
-        System.out.println("String: "); Clavier.lireString();
-        System.out.println("long: "); Clavier.lire_long();
-        System.out.println("float: "); Clavier.lire_float();
-        System.out.println("String: "); Clavier.lireString();
-        System.out.println("int: "); Clavier.lire_int();
-        System.out.println("short: "); Clavier.lire_short();
-        System.out.println("char: "); Clavier.lire_char();
-        System.out.println("double: "); Clavier.lire_double();
-        System.out.println("byte: "); Clavier.lire_byte();
+        System.out.print("boolean: " );System.out.println( Clavier.lire_boolean());
+        System.out.print("String: " );System.out.println( Clavier.lireString());
+        System.out.print("long: " );System.out.println( Clavier.lire_long());
+        System.out.print("float: " );System.out.println( Clavier.lire_float());
+        System.out.print("String: " );System.out.println( Clavier.lireString());
+        System.out.print("int: " );System.out.println( Clavier.lire_int());
+        System.out.print("short: " );System.out.println( Clavier.lire_short());
+        System.out.print("char: " );System.out.println( Clavier.lire_char());
+        System.out.print("double: " );System.out.println( Clavier.lire_double());
+        System.out.print("byte: " );System.out.println( Clavier.lire_byte());
     }
 }
